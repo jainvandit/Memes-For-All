68337bb5a9c9f75f7726fde8db3adf99e11f9c2f
jainvandit <mailvandit.j@gmail.com>
2019-11-26 20:19:55 +0530

    Mark 2: Added Main View List Views

---
+++ Memes For All/mainTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  mainTableViewCell.swift
+//  Memes For All
+//
+//  Created by Vandit Jain on 26/11/19.
+//  Copyright © 2019 jainvandit. All rights reserved.
+//
+
+import UIKit
+
+class mainTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
